4221d407aabc8618623dff23a38b1adc39970f17
om4mca <om4mca@gmail.com>
2026-07-02 16:15:15 +0530

    Add HomeWorkExtra.py for employee salary calculation

---
+++ Python/Day01/HomeWorkExtra.py
@@ -0,0 +1,11 @@
+name= input("Enter your name: ")
+salary= float(input("Enter salary: "))
+bonus= float(input("Enter bonus: "))
+total_salary= salary + bonus
+
+print()
+print("------Employee Information--------")
+print("--------------")
+print("Name: ", name)
+print("Total Salary: ", total_salary)
+
